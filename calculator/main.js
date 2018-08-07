64e74f488eecfac2d6e957d1acc37fd8c15d55e2
annaoleynik <prigmaster@gmail.com>
2018-08-06 23:40:37 -0700

    refuctoring + bug fix

--- calculator/main.js
+++ calculator/main.js
@@ -1,31 +1,55 @@
 
 let operators = ['+', '-', '/', '*'];
-function invokeCommand(command) {
-    let display = document.getElementById('display');
-    if (command === '=') {
-        let result = calculate(display.textContent);
-        display.textContent= result ;
+let display = document.getElementById('display');
+setValue('0');
 
-    } else {
-        displayValue(command, display);
+function addNumber(num) {
+    let displayText = getDisplayText();
+    let firstChar = displayText[0];
+    if (firstChar === '0' && displayText.length === 1) {
+        setValue(num);
+        return;
     }
-
+    addValue(num);
 }
 
-
-function displayValue(value, display) {
-    let lastCurrentChart = display.textContent[display.textContent.length -1];
-    if (operators.includes(value) && operators.includes(lastCurrentChart)) {
-        if (value !== lastCurrentChart) {
-           display.textContent =  display.textContent.slice(0, -1) + value;
+function addOperation(operator) {
+    let displayText = getDisplayText();
+    let lastCurrentChart = displayText[displayText.length -1];
+    if (operators.includes(operator) && operators.includes(lastCurrentChart)) {
+        if (operator !== lastCurrentChart) {
+            setValue(displayText.slice(0, -1) + operator);
         }
         return;
     }
+    addValue(operator);
+}
+
+function clearDisplay() {
+    setValue('0');
+}
+
+function calculate() {
+    let expretion = getDisplayText();
+    let result = new Function('return ' + expretion + ';')();
+    setValue(result);
+}
+
+function addValue(value) {
     display.textContent += value;
-
-
 }
 
-function calculate(expretion) {
-    return new Function('return ' + expretion + ';')();
+function setValue(value) {
+    display.textContent = value;
 }
+
+function getDisplayText() {
+    return display.textContent;
+}
+
+
+
+
+
+
+
